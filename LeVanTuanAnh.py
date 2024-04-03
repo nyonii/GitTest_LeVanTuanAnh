75eba3ac5c1cc6b33d8201ee23c8de6a1d30e614
nyonii <levantuananh08@gmail.com>
2024-04-03 17:37:42 +0700

    lan 2

--- LeVanTuanAnh.py
+++ LeVanTuanAnh.py
@@ -6,3 +6,19 @@
 # thành
 # nam đế cư
 # nam quốc sơn hà
+
+# Đọc tệp văn bản và lưu nội dung vào một danh sách
+file_path = 'ten_file.txt'  # Thay đổi đường dẫn tệp văn bản tại đây
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+
+# In các dòng trong danh sách theo thứ tự ngược lại
+for line in reversed(lines):
+    print(line.strip())  # Xóa các ký tự trắng không cần thiết ở đầu và cuối dòng# Đọc tệp văn bản và lưu nội dung vào một danh sách
+file_path = 'ten_file.txt'  # Thay đổi đường dẫn tệp văn bản tại đây
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+
+# In các dòng trong danh sách theo thứ tự ngược lại
+for line in reversed(lines):
+    print(line.strip())  # Xóa các ký tự trắng không cần thiết ở đầu và cuối dòng
